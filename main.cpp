e5175e55c0bb78bc74b00cd307a3c33ef1e813de
Steven Vch <unstatik@staremax.com>
2016-09-27 20:49:02 +0300

    support load style from external file; close #1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include <QStyle>
 #include <QDesktopWidget>
 #include <QTranslator>
+#include <QFile>
+#include <QTextStream>
 
 const QString RouletteSourceStorage::TEXT_LIST_CONFIG_KEY = "text-list";
+const QString appStyleFile = "style/general.css";
 
 void showWindowAtCenter(SRWindow *window)
 {
@@ -26,6 +29,46 @@ void configureSRWindow(SRWindow *srWindow)
     srWindow->setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint);
 }
 
+QString failBackStyle()
+{
+    return " "
+           "QWidget#mainWindow  {"
+                                 "background-color: #000000;"
+                                "}"
+           "QLabel#rollValue  {"
+                                 "color: #ffffff;"
+                              "}"
+
+           "QPushButton#rollButton {"
+                                    "border: 1px solid #ef9327;"
+                                    "border-radius: 6px;"
+                                    "background-color: #116a06;"
+                                    "background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 0.67, stop: 0 #e5f2f7, stop: 1 #bbe4ef);"
+                                    "}"
+           "QPushButton#rollButton:pressed {"
+                                    "border: 1px solid #989898;"
+                                    "background-color: #222222;"
+                                    "background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 0.67, stop: 0  #787878, stop: 1 #989898);"
+                                   "}";
+}
+
+void loadStyle(QApplication *srApp)
+{
+    QString style = srApp->styleSheet();
+    QString appCss = "";
+
+    QFile file(appStyleFile);
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        appCss = failBackStyle();
+    } else {
+        QTextStream in(&file);
+        appCss = in.readAll();
+        file.close();
+    }
+
+    srApp->setStyleSheet(appCss);
+}
+
 int main(int argc, char *argv[])
 {
     QApplication srApp(argc, argv);
@@ -36,6 +79,7 @@ int main(int argc, char *argv[])
     //srTranslator.load("sr_ru_EN");
 
     srApp.installTranslator(&srTranslator);
+    loadStyle(&srApp);
 
     RouletteSourceStorage rss;
     rss.fillTextSourcesFromConfig("sources.ini");
